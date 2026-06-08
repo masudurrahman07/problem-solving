f9b32f73743916fa4f0ce411ea0ff57d62e45443
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-08 15:37:47 +0600

    second problem solved

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+// Problem 1 
+
 function swapVariables(a, b) {
-   
+   [a, b] = [b, a];
+   return [a, b];
+}
+ 
+// console.log(swapVariables(5, 10)) 
+
+
+// Problem 2
+
+function checkEvenOdd(num) {
+    return num % 2 === 0 ? "Even" : "Odd";
 }
 
+// console.log(checkEvenOdd(7)); 
+ 
