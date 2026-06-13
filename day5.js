542ea34f53c0ba95ce76ed5a38ef03c7c0cb1f5e
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-13 16:09:47 +0600

    solved 21 problem

---
+++ day5.js
@@ -0,0 +1,14 @@
+// Problem 21: Factorial (Recursive)  [Easy]
+
+ function factorial(n) {
+    if ( n === 0){
+        return 1;
+    }
+    return factorial(n - 1) * n;
+ }
+
+//  console.log(factorial(5)); // Output: 120
+
+  
+// Problem 22: Fibonacci Sequence  [Easy]
+
