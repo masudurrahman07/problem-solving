4478eb9312ae303f84482f2bf1f6e7c8c3806c90
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-13 16:34:45 +0600

    solved problem 23

--- day5.js
+++ day5.js
@@ -12,3 +12,43 @@
   
 // Problem 22: Fibonacci Sequence  [Easy]
 
+function fibonacci(n) {
+   if (n === 0 || n === 1) { 
+    return n;
+   }
+ let prev = 0;
+ let curr = 1;
+
+ for ( let i = 2; i <=n; i++){
+    let next = prev + curr; 
+    prev = curr;
+    curr = next;
+ }
+ return curr;
+}
+
+// console.log(fibonacci(6)); // Output: 8
+
+// Problem 23: Create a Counter with Closure  [Medium]
+
+ function makeCounter(){
+    let count = 0; 
+    return {
+        increment: function() {
+            count++;
+        },
+
+        decrement: function() {
+            count--;
+        },
+
+        getCount: function() {
+            return count;
+        }
+    }
+ }
+
+ const c = makeCounter();c.increment(); c.increment();c.getCount();
+//  console.log(c.getCount()); 
+
+ 
