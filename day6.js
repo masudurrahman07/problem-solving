9c2626700af793b25e12c42344e488d1b33005d2
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-14 15:42:58 +0600

    solved 27 problem

--- day6.js
+++ day6.js
@@ -2,7 +2,20 @@
 
 const user = {firstName:'Sara', lastName:'Khan', age:25};// Extract: name='Sara', lastName='Khan', age=25
  const { firstName: name, lastName, age } = user;
- 
+
 // console.log(`name='${name}', lastName='${lastName}', age=${age}`);
 
  
+// Problem 27: Merge Arrays with Spread  [Easy]
+// Description: Write a function mergeArrays(...arrays) that accepts any number of arrays and merges them into one using the spread operator.
+// Example:
+// Input: [1,2], [3,4], [5]Output: [1,2,3,4,5]
+// Hint: Use rest parameters and spread inside reduce or flat.
+
+function mergeArrays(...arrays) {
+     return arrays.flat();
+}
+
+
+const merged = mergeArrays([1, 2], [3, 4], [5]);
+// console.log(merged); 
