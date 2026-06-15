6af6816240e84e2ecd5194799fcbc2a60c851719
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-15 17:33:03 +0600

    trying to solve 32

--- day7.js
+++ day7.js
@@ -4,15 +4,56 @@
 // const debouncedSearch = debounce(search, 300);// Fires only once after user stops typing for 300ms
 // Hint: Use clearTimeout and setTimeout.
 
-const debounceFunc = (fn, delay) => {
-    setTimeout(() => {
-        fn()
+// YET TO UNDERSTAND THE LOGIC FULLY//
+
+function debounce(fn, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+
+      fn.apply(this, args);
     }, delay);
+  };
 }
 
-const fn = () => {
-    // return console.log("Hello Man!")
+
+function search(query) {
+//   console.log("Searching database for:", query);
 }
 
-const debouncedSearch = debounceFunc(fn, 300);
+
+const debouncedSearch = debounce(search, 300);
+
+
+debouncedSearch("H"); 
+debouncedSearch("Hi"); 
+
+// Output in console after 300ms: 
+// "Searching database for: Hi"
+// Problem 32: Throttle Function  [Medium]
+// Description: Write a throttle(fn, limit) function that ensures fn is called at most once every limit milliseconds.
+// Example:
+// const throttledScroll = throttle(onScroll, 200);// Fires at most once every 200ms during scroll
+// Hint: Track the last call time with Date.now().
+
+// YET TO UNDERSTAND THE LOGIC FULLY//
+
+function throttle(fn, limit) {
+    let lastCall = 0;
+    return function (...args) {
+        let now = Date.now();
+        if (now - lastCall < limit) {
+            return;
+        }
+
+        lastCall = now;
+        fn.apply(this, args);
+    };
+}
+
+
+// TO BE CONTINUED......
 
