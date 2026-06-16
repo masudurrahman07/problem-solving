86348fbba3d4e08c2a2939e9e20b7cb8e3e791d0
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-16 16:26:16 +0600

    tried to solve problem no.37

--- day8.js
+++ day8.js
@@ -15,3 +15,24 @@ function safeJsonParse(str){
 
 const test = safeJsonParse('{"a":1}')
 // console.log(test)
+
+
+
+// Problem 37: Retry a Promise  [Medium]
+// Description: Write a function retry(fn, times) that calls an async function fn up to times times, retrying if it throws an error. Resolves on first success, rejects after all attempts fail.
+// Example:
+// await retry(unstableFetch, 3);// Tries up to 3 times before failing
+// Hint: Use a loop with try/catch; only throw after all retries are exhausted.
+
+ async  function retry(fn, times){
+    let finalError;
+    for ( let i =0; i< times; i++){
+        try{
+           return await fn()
+        }
+        catch(error){
+          return finalError = error;
+        }
+    }
+    throw finalError;
+}
