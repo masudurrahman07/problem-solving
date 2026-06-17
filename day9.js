6728c98f36ecd73e1c82c9dcb052c70cb6e76d9b
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-17 17:38:43 +0600

    still trying

--- day9.js
+++ day9.js
@@ -56,9 +56,12 @@ function findMissing(arr, n) {
   const sum = (n * (n + 1)) / 2;
   let actualSum = 0;
   for (let i = 0; i < arr.length; i++) {
-  
+    // const element = arr[i ];
     actualSum += arr[i];
   }
+  //   for (let number of arr) {
+  //     actualSum += number;
+  //   }
   return sum - actualSum;
 }
-// console.log(findMissing([1, 2, 4, 5], 5));
+console.log(findMissing([1, 2, 4, 5], 5));
