eb90c8e1947c86662df656ec76d5f54c97738a8d
Masudur Rahman <rahmanmasudur557@gmail.com>
2026-06-17 17:36:12 +0600

    trying to solve 43

--- day9.js
+++ day9.js
@@ -42,3 +42,23 @@ function isAnagram(str1, str2){
 }
 
 // console.log(isAnagram ('listen', 'silent'))
+
+ 
+// Problem 43: Find Missing Number  [Easy]
+// Description: Given an array of n-1 integers from 1 to n with one number missing, write a function findMissing(arr, n) to find the missing number.
+// Example:
+// Input: [1,2,4,5], n=5  → Output: 3
+// Hint: Use the formula: expected sum = n*(n+1)/2, then subtract actual sum.
+
+// YET TO UNDERSTAND THE LOGIC//
+
+function findMissing(arr, n) {
+  const sum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+  
+    actualSum += arr[i];
+  }
+  return sum - actualSum;
+}
+// console.log(findMissing([1, 2, 4, 5], 5));
